7e94f5a573170f6d4520c9e5a53ad82f0fe9c3c5
ApplicationMaster <58971550+ApplicationMaster@users.noreply.github.com>
2022-11-01 15:58:02 +0800

    修改 groupId 名称为 medicine

--- src/main/java/com/medicine/MybatisPlusDemoApplication.java
+++ src/main/java/com/medicine/MybatisPlusDemoApplication.java
@@ -1,4 +1,4 @@
-package com.huanhuan;
+package com.medicine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/medicine/MybatisPlusDemoApplicationTests.java
+++ src/test/java/com/medicine/MybatisPlusDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.huanhuan;
+package com.medicine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
